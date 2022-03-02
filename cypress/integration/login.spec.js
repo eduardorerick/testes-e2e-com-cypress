5b05f3ab738c1465c54556bcf83f4fd594f22c7c
Eduardo Rerick <eduardorerick@gmail.com>
2022-03-02 15:53:06 -0300

    added types declarations

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,8 @@
+/// <reference path="../support/commands.d.ts" />
 it('successfully logs in', () => {
   cy.intercept('GET', '**/notes').as('getNotes')
-  cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'), { cacheSession: false })
+  cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'), {
+    cacheSession: false,
+  })
   cy.wait('@getNotes', { timeout: 15000 })
 })
